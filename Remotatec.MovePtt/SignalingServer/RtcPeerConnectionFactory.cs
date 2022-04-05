ab36b5c35b1c55935fd55c6efb611d09a899d77c
Peter Antony Rausch <peter.rausch@premiersoft.net>
2022-04-05 19:27:39 -0300

    Sender Audio and Receiver Audio

--- Remotatec.MovePtt/SignalingServer/RtcPeerConnectionFactory.cs
+++ Remotatec.MovePtt/SignalingServer/RtcPeerConnectionFactory.cs
@@ -1,26 +1,22 @@
-﻿using SIPSorcery.Media;
+﻿using System.Collections.Concurrent;
+using SIPSorcery.Media;
 using SIPSorcery.Net;
+using SIPSorceryMedia.Abstractions;
 
 namespace SignalingServer
 {
     public static class RtcPeerConnectionFactory
     {
+        private static ConcurrentDictionary<string, RTCPeerConnection> _peers = new();
+
         public static Task<RTCPeerConnection> CreatePeerConnectionAsync()
         {
             var peerConnection = CreateRtcPeerConnection();
+            _peers.TryAdd(peerConnection.SessionID, peerConnection);
 
-            var audioSource = SetupAudioEndpoint();
+            AddMediaToPeerConnection(peerConnection);
 
-            AddMediaToPeerConnection(audioSource, peerConnection);
-
-            ConfigureAudioEventHandlers(audioSource, peerConnection);
-
-            ConfigurePeerConnectionEventHandlers(peerConnection, audioSource);
-
-            peerConnection.OnAudioFormatsNegotiated += audioFormats =>
-            {
-                audioSource.SetAudioSourceFormat(audioFormats.First());
-            };
+            ConfigurePeerConnectionEventHandlers(peerConnection);
 
             return Task.FromResult(peerConnection);
         }
@@ -52,20 +48,22 @@ namespace SignalingServer
             return new AudioExtrasSource(new AudioEncoder(), new AudioSourceOptions { AudioSource = AudioSourcesEnum.Music });
         }
 
-        private static void AddMediaToPeerConnection(AudioExtrasSource audioSource, RTCPeerConnection peerConnection)
+        private static void AddMediaToPeerConnection(RTCPeerConnection peerConnection)
         {
-            var audioTrack = new MediaStreamTrack(audioSource.GetAudioSourceFormats(), MediaStreamStatusEnum.SendRecv);
+            var audioTrack = new MediaStreamTrack(
+                SDPMediaTypesEnum.audio,
+                false,
+                new List<SDPAudioVideoMediaFormat>
+                {
+                    new SDPAudioVideoMediaFormat(SDPWellKnownMediaFormatsEnum.PCMU)
+                }
+            );
             peerConnection.addTrack(audioTrack);
         }
 
-        private static void ConfigureAudioEventHandlers(AudioExtrasSource audioSource, RTCPeerConnection peerConnection)
+        private static void ConfigurePeerConnectionEventHandlers(RTCPeerConnection peerConnection)
         {
-            audioSource.OnAudioSourceEncodedSample += peerConnection.SendAudio;
-        }
-
-        private static void ConfigurePeerConnectionEventHandlers(RTCPeerConnection peerConnection, AudioExtrasSource audioSource)
-        {
-            peerConnection.onconnectionstatechange += async state => await OnConnectionStateChanged(state, audioSource, peerConnection);
+            peerConnection.onconnectionstatechange += async state => await OnConnectionStateChanged(state, peerConnection);
 
             // Diagnostics.
             peerConnection.OnReceiveReport += (re, media, rr) => Console.WriteLine($"RTCP Receive for {media} from {re}\n{rr.GetDebugSummary()}");
@@ -73,27 +71,33 @@ namespace SignalingServer
             peerConnection.GetRtpChannel().OnStunMessageReceived += (msg, ep, isRelay) => Console.WriteLine($"STUN {msg.Header.MessageType} received from {ep}.");
             peerConnection.oniceconnectionstatechange += (state) => Console.WriteLine($"ICE connection state change to {state}.");
 
-            peerConnection.OnRtpPacketReceived += PeerConnection_OnRtpPacketReceived;
+            peerConnection.OnRtpPacketReceived += (rep, media, rtpPkt) => PeerConnection_OnRtpPacketReceived(rep, media, rtpPkt, peerConnection);
         }
 
-        private static void PeerConnection_OnRtpPacketReceived(System.Net.IPEndPoint rep, SDPMediaTypesEnum media, RTPPacket rtpPkt)
+        private static void PeerConnection_OnRtpPacketReceived(System.Net.IPEndPoint rep, SDPMediaTypesEnum mediaType, RTPPacket rtpPkt, RTCPeerConnection peerConnection)
         {
             Console.WriteLine("RtpPackatReceived...");
+
+            foreach (var peer in _peers)
+            {
+                if (peer.Key != peerConnection.SessionID)
+                {
+                    peer.Value.SendRtpRaw(mediaType, rtpPkt.Payload, rtpPkt.Header.Timestamp, rtpPkt.Header.MarkerBit, rtpPkt.Header.PayloadType);
+                }
+            }
         }
 
-        private static async Task OnConnectionStateChanged(RTCPeerConnectionState state, AudioExtrasSource audioSource, RTCPeerConnection peerConnection)
+        private static async Task OnConnectionStateChanged(RTCPeerConnectionState state, RTCPeerConnection peerConnection)
         {
             Console.WriteLine($"Peer connection state change to {state}.");
 
             if (state == RTCPeerConnectionState.connected)
             {
-                await audioSource.StartAudio();
                 return;
             }
 
             if (state == RTCPeerConnectionState.closed)
             {
-                await audioSource.CloseAudio();
                 return;
             }
 
